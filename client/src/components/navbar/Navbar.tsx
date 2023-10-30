fc25f8ecc3eb42e067efe3e71c2efc145d533643
marfogped <martin@poreldeporte.com>
2023-10-30 11:15:41 -0300

    feat: mobile styles & viewport config

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,24 +1,42 @@
 import { Link } from "react-router-dom";
 import { NAV_ITEMS } from "../../constants";
+import { Bars2Icon } from "@heroicons/react/24/solid";
+import useWindowDimensions from "../../hooks/useWindowDimentions";
 
 const Navbar = () => {
+  const { windowWidth } = useWindowDimensions();
+
   return (
     <nav className="flex items-center w-full p-8 fixed">
       <ul className="flex items-center justify-between w-full">
-        {NAV_ITEMS
-          ? NAV_ITEMS.map((item) => (
-              <li
-                key={item.name}
-                className={`text-8xl font-semibold ${
-                  item.name === "CONTACT" ? "stroke-font" : "text-white"
-                }`}
-              >
-                <Link to={item.to}>
-                  <span>{item.name}</span>
-                </Link>
-              </li>
-            ))
-          : ""}
+        {windowWidth < 480 ? (
+          <>
+            <li className="text-4xl font-semibold text-white">
+              <span>HNGS</span>
+            </li>
+            <li className="text-8xl font-semibold text-white">
+              {" "}
+              <Bars2Icon className="h-12" />{" "}
+            </li>
+          </>
+        ) : (
+          <>
+            {NAV_ITEMS
+              ? NAV_ITEMS.map((item) => (
+                  <li
+                    key={item.name}
+                    className={`sm:text-5xl lg:text-6xl xl:text-8xl font-semibold ${
+                      item.name === "CONTACT" ? "stroke-font" : "text-white"
+                    }`}
+                  >
+                    <Link to={item.to}>
+                      <span>{item.name}</span>
+                    </Link>
+                  </li>
+                ))
+              : ""}
+          </>
+        )}
       </ul>
     </nav>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: { max: "480px" },
+        sm: { min: "481px" },
+        md: { min: "768px" },
+        lg: { min: "992px" },
+        xl: { min: "1200px" },
+        xxl: { min: "1920px" },
+      },
     },
   },
   plugins: [],
-}
-
+};
