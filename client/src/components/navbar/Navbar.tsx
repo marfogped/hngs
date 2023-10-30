283542b362b45f6a7eb800b9d31a7f3b9d37ebd4
marfogped <martin@poreldeporte.com>
2023-10-30 13:09:10 -0300

    feat: navbar onScroll styles

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { NAV_ITEMS } from "../../constants";
 import { Bars2Icon } from "@heroicons/react/24/solid";
@@ -5,16 +6,44 @@ import useWindowDimensions from "../../hooks/useWindowDimentions";
 
 const Navbar = () => {
   const { windowWidth } = useWindowDimensions();
+  const [color, setColor] = useState<boolean>(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 60) {
+      setColor(true);
+    }
+    if (window.scrollY === 0) {
+      setColor(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+    };
+  }, []);
 
   return (
-    <nav className="flex items-center w-full p-8 fixed">
+    <nav
+      className={`${
+        color ? "bg-white shadow-md delay-[400ms]" : ""
+      } flex items-center w-full p-8 fixed transition-all ease-in`}
+    >
       <ul className="flex items-center justify-between w-full">
         {windowWidth < 480 ? (
           <>
-            <li className="text-4xl font-semibold text-white">
+            <li
+              className={`${
+                color ? "text-black" : "text-white"
+              } text-4xl font-semibold`}
+            >
               <span>HNGS</span>
             </li>
-            <li className="text-8xl font-semibold text-white">
+            <li
+              className={`${
+                color ? "text-black" : "text-white"
+              } text-8xl font-semibold`}
+            >
               {" "}
               <Bars2Icon className="h-12" />{" "}
             </li>
@@ -25,8 +54,18 @@ const Navbar = () => {
               ? NAV_ITEMS.map((item) => (
                   <li
                     key={item.name}
-                    className={`sm:text-5xl lg:text-6xl xl:text-8xl font-semibold ${
-                      item.name === "CONTACT" ? "stroke-font" : "text-white"
+                    className={`font-semibold ${
+                      item.name === "CONTACT"
+                        ? `${
+                            color
+                              ? "text-black xl:text-6xl"
+                              : "text-white sm:text-5xl lg:text-6xl xl:text-8xl"
+                          } stroke-font transition-all ease-in duration-500`
+                        : `${
+                            color
+                              ? "text-black xl:text-6xl"
+                              : "text-white sm:text-5xl lg:text-6xl xl:text-8xl"
+                          } transition-all ease-in duration-500`
                     }`}
                   >
                     <Link to={item.to}>
