c488bee2d9074b591be7afff01d697b9d3477073
Martin Fogliacco <fogliaccomartin3@gmail.com>
2023-10-31 02:52:05 -0300

    fix: styles at footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -15,7 +15,7 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ data }) => {
   return (
-    <footer className="flex xs:flex-col sm:flex-col xs:justify-start items-center md:justify-between p-6 border-t border-gray-500">
+    <footer className="flex xs:flex-col sm:flex-col md:flex-row xs:justify-start items-center md:justify-between p-6 border-t border-gray-500">
       <div>
         <ul className="flex items-center gap-6">
           {FOOTER_ITEMS
