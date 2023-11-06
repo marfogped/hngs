2d3d722899f7362492dabbb386b321837cbd7548
marfogped <martin@poreldeporte.com>
2023-11-06 12:05:19 -0300

    add: Loading component & Routing without transition

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,26 @@
 import { HomeContainer, OfficeContainer, WorkContainer } from "./containers";
 import { Navbar, NotFound, Footer, ProjectDetails } from "./components";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Transition from "./components/transition/Transition";
-import { motion, AnimatePresence } from "framer-motion";
+import { useSanity } from "./hooks/useSanity";
 
 function App() {
   const location = useLocation();
+  const { fetchError, isLoading } = useSanity();
 
   return (
     <>
-      <AnimatePresence mode="wait">
-        <motion.main key={location.pathname} className="h-full">
-          <Navbar />
-          <Transition />
-          <Routes 
-          location={location} 
-          key={location.pathname}
-          >
-            <Route
-              index
-              element={<HomeContainer />}
-              />
-            <Route
-              path="/work"
-              element={<WorkContainer />}
-              />
+      {!fetchError || isLoading ? <Navbar /> : ""}
+      <main className="h-full">
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<HomeContainer />} />
+          <Route path="/work" element={<WorkContainer />} />
 
-            <Route
-              path="/office"
-              element={<OfficeContainer />}
-            />
-            <Route path="/work/:project" element={<ProjectDetails />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </motion.main>
-      </AnimatePresence>
+          <Route path="/office" element={<OfficeContainer />} />
+          <Route path="/work/:project" element={<ProjectDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {!fetchError || isLoading ? <Footer /> : ""}
+      </main>
     </>
   );
 }
