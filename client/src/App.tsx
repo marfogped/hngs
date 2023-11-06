0499b9b00d429349dd634631288a6fc44885f6d1
marfogped <martin@poreldeporte.com>
2023-11-06 12:24:39 -0300

    add: /contact routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,9 @@
-import { HomeContainer, OfficeContainer, WorkContainer } from "./containers";
+import {
+  HomeContainer,
+  OfficeContainer,
+  WorkContainer,
+  ContactContainer,
+} from "./containers";
 import { Navbar, NotFound, Footer, ProjectDetails } from "./components";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { useSanity } from "./hooks/useSanity";
@@ -14,8 +19,8 @@ function App() {
         <Routes location={location} key={location.pathname}>
           <Route index element={<HomeContainer />} />
           <Route path="/work" element={<WorkContainer />} />
-
           <Route path="/office" element={<OfficeContainer />} />
+          <Route path="/contact" element={<ContactContainer />} />
           <Route path="/work/:project" element={<ProjectDetails />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- client/src/containers/index.ts
+++ client/src/containers/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeContainer } from "./home-container/HomeContainer";
 export { default as OfficeContainer } from "./office-container/OfficeContainer";
 export { default as WorkContainer } from "./work-container/WorkContainer";
+export { default as ContactContainer } from "./contact-container/ContactContainer";
